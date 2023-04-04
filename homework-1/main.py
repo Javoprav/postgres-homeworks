e626b5cd8a2757876286b40718ae3bfffaf2391b
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-04-04 23:06:41 +0300

    add create_tables.sql add main

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,18 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import os, psycopg2
+
+if __name__ == '__main__':
+    conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='12345'
+    )
+
+    cur = conn.cursor()
+    customers_data = os.path.join('north_data', 'customers_data.csv')
+    employees_data = os.path.join('north_data', 'employees_data.csv')
+    orders_data = os.path.join('north_data', 'orders_data.csv')
+    f = open(employees_data, 'r')
+    cur.copy_from(f, 'employees', sep=',')
+    f.close()
