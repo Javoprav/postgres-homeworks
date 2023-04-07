d3d3eecf3ba45c02141832d57b6baa965e4ae0a5
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-04-07 22:38:52 +0300

    ADD DISTINCT

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -6,7 +6,7 @@ FROM customers;
 SELECT order_id, (shipped_date - order_date) as diff
 FROM orders;
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
-SELECT city DISTINCT
+SELECT DISTINCT city
 FROM customers;
 -- 4. количество заказов (таблица orders)
 SELECT COUNT(*)
